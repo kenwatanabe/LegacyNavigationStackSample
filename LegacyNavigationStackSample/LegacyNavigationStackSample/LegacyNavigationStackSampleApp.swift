1ac6f418c12b32c53f2a9078f001051bea1204b8
kenji.watanabe <kenji.www7@gmail.com>
2025-02-14 13:04:45 +0900

    init

---
+++ LegacyNavigationStackSample/LegacyNavigationStackSample/LegacyNavigationStackSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyNavigationStackSampleApp.swift
+//  LegacyNavigationStackSample
+//
+//  Created by kenjiwatanabe on 2025/02/14.
+//
+
+import SwiftUI
+
+@main
+struct LegacyNavigationStackSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LegacyNavigationStackSample/LegacyNavigationStackSampleTests/LegacyNavigationStackSampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyNavigationStackSampleTests.swift
+//  LegacyNavigationStackSampleTests
+//
+//  Created by kenjiwatanabe on 2025/02/14.
+//
+
+import Testing
+@testable import LegacyNavigationStackSample
+
+struct LegacyNavigationStackSampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
